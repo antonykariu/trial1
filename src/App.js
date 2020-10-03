313f649c20b9cc7bb519dcb8b8ab45114eec3cba
Antony_Kariu <kariuantony@gmail.com>
2020-10-03 13:40:32 +0300

    Adds Homepage and 404 page,
    Created components for the homepage and 404 page routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="*" component={NotFound} />
+    </Switch>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Suspense>
+  </Router>,
+  document.getElementById("root")
 );
-
